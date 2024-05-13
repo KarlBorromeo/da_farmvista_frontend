ba732377262ba5ff6650c8fe619246843d568b6c
KarlBorromeo <lrakborromeo@gmail.com>
2024-05-13 13:10:14 +0800

    separated the manual survey button component

---
+++ components/authenticated/buttons/manualSurveyButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-dialog v-model="dialog" width="700">
+    <template v-slot:activator="{ on, attrs }">
+        <v-btn block color="success" large v-bind="attrs" v-on="on">
+        <v-icon class="mr-2">mdi-text-box-edit-outline</v-icon>
+        MANUAL SURVEY
+        </v-btn>
+    </template>
+    <questionnaire-vue />
+    </v-dialog>
+</template>
+
+<script>
+import QuestionnaireVue from '~/components/authenticated/modal/questionnaire.vue'
+export default {
+    components: { QuestionnaireVue },
+    data(){
+        return{
+            dialog: false,
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- pages/profiling.vue
+++ pages/profiling.vue
@@ -11,15 +11,7 @@
           <upload-button />
         </v-col>
         <v-col cols="12" sm="6" class="pt-0">
-          <v-dialog v-model="dialog" width="700">
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn block color="success" large v-bind="attrs" v-on="on">
-                <v-icon class="mr-2">mdi-text-box-edit-outline</v-icon>
-                MANUAL SURVEY
-              </v-btn>
-            </template>
-            <questionnaire-vue />
-          </v-dialog>
+          <manual-survey-button />
         </v-col>
       </v-row>
     </v-container>
@@ -28,13 +20,12 @@
 
 <script>
 import TabDescription from '~/components/authenticated/TabDescription.vue'
-import QuestionnaireVue from '~/components/authenticated/modal/questionnaire.vue'
 import UploadButton from '~/components/authenticated/buttons/uploadButton.vue'
+import ManualSurveyButton from '~/components/authenticated/buttons/manualSurveyButton.vue'
 export default {
-  components: { TabDescription, QuestionnaireVue, UploadButton },
+  components: { TabDescription, UploadButton, ManualSurveyButton },
   data() {
     return {
-      dialog: false,
       description:
         'This module simplifies farm profiling by offering easy Excel uploads or manual survey input, allowing  to capture comprehensive data about coffee farmers.',
     }
